4910bf9be9d0926595af5feeef773700512d757f
Phaneendra <phaniovv@gmail.com>
2026-07-02 15:33:54 +0530

    Eigth Commit

--- server.ts
+++ server.ts
@@ -7,7 +7,7 @@ import { GoogleGenAI } from "@google/genai";
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 5173;
 
 // Parse JSON bodies
 app.use(express.json());
